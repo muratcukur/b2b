7bffd9d33193745750971cd6557d61781acba206
Murat Cukur <murattcukur@gmail.com>
2021-07-23 21:06:28 +0300

    redis added for selenium

--- worker.py
+++ worker.py
@@ -1,5 +1,4 @@
 import os
-import urlparse
 import redis
 from rq import Worker, Queue, Connection
 
@@ -7,9 +6,7 @@ listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL')
 
-urlparse.uses_netloc.append('redis')
-url = urlparse.urlparse(redis_url)
-conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
